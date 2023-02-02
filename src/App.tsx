2df694a98c0139ed4cbf2ac88953ae11a9a1bf31
ArtyomKub <kub.killjoy@gmail.com>
2023-02-02 22:29:16 +0300

    Complete my 3D Rotating Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import './App.css';
+import style from './App.module.css'
 
 function App() {
-  return (
-    <div >
-
-    </div>
-  );
+    return (
+        <div className={style.container}>
+            <div className={style.carousel}>
+                <div className={style.image}><span>THIS IS</span></div>
+                <div className={style.image}><span>MY FIRST</span></div>
+                <div className={style.image}><span>3D ANIMATION</span></div>
+                <div className={style.image}><span>PROJECT</span></div>
+                <div className={style.image}><span>WITH</span></div>
+                <div className={style.image}><span>TYPESCRIPT</span></div>
+                <div className={style.image}><span>PRODUCT</span></div>
+                <div className={style.image}><span>BY</span></div>
+                <div className={style.image}><span>ARTYOM KUB</span></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
